810d1fff5276cf318639e47d382c935db239a733
yin070406 <64211270+yin070406@users.noreply.github.com>
2022-09-22 00:34:18 +0800

    Add files via upload

---
+++ js/redirect.js
@@ -0,0 +1,6 @@
+function Redirect() 
+    {  
+        window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
+    } 
+    document.write("You will be redirected to a new page in 5 seconds"); 
+    setTimeout('Redirect()', 5000);  
