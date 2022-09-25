bc17f17030475c0d0015a3fc9697b26f44d11c1a
yin070406 <64211270+yin070406@users.noreply.github.com>
2022-09-25 17:50:42 +0800

    Update and rename js/redirect.js to text.js

--- js/redirect.js
+++
@@ -1,6 +0,0 @@
-function Redirect() 
-    {  
-        window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
-    } 
-    document.write("You will be redirected to a new page in 5 seconds"); 
-    setTimeout('Redirect()', 5000);  
